c9e8c436cf3856375ce94ed31cb4a36548732e02
kazuma1989 <kazuma.ebina@quipper.com>
2020-07-31 12:30:21 +0900

    refactor: 無理やり Firebase SDK の型を参照していたのを改善

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,10 @@
-import firebase from "firebase"
+/// <reference types="firebase" />
+
 import "/__/firebase/7.17.1/firebase-app.js"
 import "/__/firebase/7.17.1/firebase-auth.js"
 import "/__/firebase/7.17.1/firebase-firestore.js"
 import "/__/firebase/7.17.1/firebase-storage.js"
 import "/__/firebase/init.js"
-
-// unused import を残すため
-type _ = firebase.app.App
 
 export const app = self.firebase.app
 export const auth = self.firebase.auth
