b8a35b34c3dc8c82a1153cb8d570590e01e735b3
kazuma1989 <kazuma.ebina@quipper.com>
2020-08-08 10:10:33 +0900

    feat: games collection を作成できるように

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,11 @@
+import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
 
-export const helloWorld = functions.https.onRequest((req, resp) => {
-  functions.logger.info("Hello logs!", { structuredData: true })
+const app = admin.initializeApp()
+const db = app.firestore()
+
+export const setupGame = functions.https.onRequest(async (req, resp) => {
+  await db.collection("games").doc("1xNV05bl2ISPqgCjSQTq").create({})
 
   resp.send("Hello from Firebase!")
 })
