ac0208e2d40efd94bea81f57b3293ecbf008d825
kazuma1989 <kazuma.ebina@quipper.com>
2020-08-08 12:18:35 +0900

    chore: local package として data モジュール追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
+import * as data from "data/shinkei-suijaku.json"
 
 const app = admin.initializeApp()
 const db = app.firestore()
 
 export const setupGame = functions.https.onRequest(async (req, resp) => {
-  await db.collection("games").doc("1xNV05bl2ISPqgCjSQTq").create({})
+  // await db.collection("games").doc("1xNV05bl2ISPqgCjSQTq").create({})
+
+  functions.logger.debug(data)
 
   resp.send("Hello from Firebase!")
 })
