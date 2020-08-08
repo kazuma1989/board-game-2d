1563650d9761b37294eaf652eba99315d8bde677
kazuma1989 <kazuma.ebina@quipper.com>
2020-08-08 19:02:19 +0900

    feat: 52 枚のカードを並べる初期化ができるように

--- functions/src/data/shinkei-suijaku.ts
+++ functions/src/data/shinkei-suijaku.ts
@@ -1,39 +1,67 @@
+import { randomId, shuffle } from "../util"
+import { allCards as _allCards } from "./allCards"
+
 export default function data() {
+  const piles: Record<
+    string,
+    {
+      col: number
+      row: number
+      cards: {
+        id: string
+        surface: "back" | "face"
+        text: string
+        src: {
+          back: string
+          face: string
+        }
+      }[]
+    }
+  > = {}
+
+  const allCards = [..._allCards]
+  shuffle(allCards)
+
+  const cr$ = colAndRow()
+  let r = cr$.next()
+  while (!r.done) {
+    const [i, { col, row }] = r.value
+
+    const card = allCards[i % allCards.length]
+    piles[[col, row].join(",")] = {
+      col,
+      row,
+      cards: [
+        {
+          ...card,
+          id: randomId(),
+          surface: "back",
+        },
+      ],
+    }
+
+    r = cr$.next()
+  }
+
   return {
-    piles: {
-      "10,22": {
-        col: 10,
-        row: 22,
-        cards: [
-          {
-            id: "47dkdytrgbDda",
-            src: {
-              back: "back.png",
-              face: "face.png",
-            },
-            surface: "face",
-            text: "",
-          },
-        ],
-      },
-      "22,10": {
-        col: 22,
-        row: 10,
-        cards: [
-          {
-            id: "47DKDYTRGBDDA",
-            src: {
-              back: "back.png",
-              face: "face.png",
-            },
-            surface: "back",
-            text: "",
-          },
-        ],
-      },
-    },
-    "piles/10,22/metas": {
-      xxxx: {},
-    },
+    piles,
+  }
+}
+
+function* colAndRow() {
+  let index = 0
+  let col = 13
+
+  while (col <= 25) {
+    let row = 16
+
+    while (row <= 23) {
+      yield [index, { col, row }] as const
+
+      row += 2
+      index += 1
+    }
+
+    col += 1
   }
 }
