b367a78c9d94e726179fb54785f403005dff9650
kazuma1989 <funifuni.1204@gmail.com>
2020-08-12 08:56:27 +0900

    refactor: strictFunctionTypes 有効化

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,4 +1,5 @@
 import produce from "https://cdn.skypack.dev/immer"
+import { Reducer } from "redux"
 import { byId, randomId } from "./util.js"
 
 export type State = {
@@ -149,155 +150,158 @@ export type Action =
       type: "Game.IdUnset"
     }
 
-export const reducer = produce((draft: State, action: Action) => {
-  switch (action.type) {
-    case "Firestore.ChangePiles": {
-      const { changes } = action.payload
+export const reducer: Reducer<State, Action> = produce(
+  (draft: State, action: Action) => {
+    switch (action.type) {
+      case "Firestore.ChangePiles": {
+        const { changes } = action.payload
 
-      changes.forEach(change => {
-        switch (change.type) {
-          case "added":
-          case "modified": {
-            const { id: pileId, data } = change
-            if (!isPileData(data)) return
+        changes.forEach(change => {
+          switch (change.type) {
+            case "added":
+            case "modified": {
+              const { id: pileId, data } = change
+              if (!isPileData(data)) return
 
-            const pile = {
-              ...data,
-              id: pileId as Pile["id"],
+              const pile = {
+                ...data,
+                id: pileId as Pile["id"],
+              }
+
+              const target = draft.piles.find(byId(pileId))
+              if (target) {
+                draft.piles[draft.piles.indexOf(target)] = pile
+              } else {
+                draft.piles.push(pile)
+              }
+
+              return
             }
 
-            const target = draft.piles.find(byId(pileId))
-            if (target) {
-              draft.piles[draft.piles.indexOf(target)] = pile
-            } else {
-              draft.piles.push(pile)
+            case "removed": {
+              const { id: pileId } = change
+
+              const target = draft.piles.find(byId(pileId))
+              if (!target) return
+
+              draft.piles.splice(draft.piles.indexOf(target), 1)
+
+              return
             }
-
-            return
           }
+        })
 
-          case "removed": {
-            const { id: pileId } = change
+        return
+      }
 
-            const target = draft.piles.find(byId(pileId))
-            if (!target) return
+      case "Card.DoubleTap": {
+        const { cardId, nextSurface, timestamp } = action.payload
 
-            draft.piles.splice(draft.piles.indexOf(target), 1)
-
-            return
+        if (draft.tempCardSurface[cardId]?.timestamp ?? -1 <= timestamp) {
+          draft.tempCardSurface[cardId] = {
+            surface: nextSurface,
+            timestamp,
           }
         }
-      })
 
-      return
-    }
-
-    case "Card.DoubleTap": {
-      const { cardId, nextSurface, timestamp } = action.payload
-
-      if (draft.tempCardSurface[cardId]?.timestamp ?? -1 <= timestamp) {
-        draft.tempCardSurface[cardId] = {
-          surface: nextSurface,
-          timestamp,
-        }
+        return
       }
 
-      return
-    }
+      case "Card.DoubleTap.Finished": {
+        const { cardId, timestamp } = action.payload
 
-    case "Card.DoubleTap.Finished": {
-      const { cardId, timestamp } = action.payload
-
-      if (draft.tempCardSurface[cardId]?.timestamp === timestamp) {
-        delete draft.tempCardSurface[cardId]
-      }
-
-      return
-    }
-
-    case "Card.MoveEnd": {
-      const { cardId, col, row, timestamp } = action.payload
-
-      if (draft.tempCardPosition[cardId]?.timestamp ?? -1 <= timestamp) {
-        draft.tempCardPosition[cardId] = { col, row, timestamp }
-      }
-
-      return
-    }
-
-    case "Card.MoveEnd.Finished": {
-      const { cardId, timestamp } = action.payload
-
-      if (draft.tempCardPosition[cardId]?.timestamp === timestamp) {
-        delete draft.tempCardPosition[cardId]
-      }
-
-      return
-    }
-
-    case "Game.IdSet": {
-      const { gameId } = action.payload
-
-      draft.game = {
-        id: gameId,
-      }
-
-      return
-    }
-
-    case "Game.IdUnset": {
-      draft.game = undefined
-      draft.piles = []
-      draft.tempCardPosition = {}
-
-      return
-    }
-
-    case "Card.AwaitTransaction": {
-      const { timestamp } = action.payload
-
-      draft.ui.runningLongTransaction.push(timestamp)
-
-      return
-    }
-
-    case "Card.AwaitTransaction.Finished": {
-      const { timestamp } = action.payload
-
-      draft.ui.runningLongTransaction = draft.ui.runningLongTransaction.filter(
-        v => v !== timestamp,
-      )
-
-      return
-    }
-
-    case "Card.ClearTempInfo": {
-      const { timestamp } = action.payload
-
-      Object.entries(draft.tempCardPosition).forEach(([cardId, temp]) => {
-        if (!temp) return
-
-        if (temp.timestamp <= timestamp) {
-          delete draft.tempCardPosition[cardId]
-        }
-      })
-
-      Object.entries(draft.tempCardSurface).forEach(([cardId, temp]) => {
-        if (!temp) return
-
-        if (temp.timestamp <= timestamp) {
+        if (draft.tempCardSurface[cardId]?.timestamp === timestamp) {
           delete draft.tempCardSurface[cardId]
         }
-      })
 
-      return
-    }
+        return
+      }
 
-    default: {
-      const _: never = action
+      case "Card.MoveEnd": {
+        const { cardId, col, row, timestamp } = action.payload
+
+        if (draft.tempCardPosition[cardId]?.timestamp ?? -1 <= timestamp) {
+          draft.tempCardPosition[cardId] = { col, row, timestamp }
+        }
+
+        return
+      }
+
+      case "Card.MoveEnd.Finished": {
+        const { cardId, timestamp } = action.payload
+
+        if (draft.tempCardPosition[cardId]?.timestamp === timestamp) {
+          delete draft.tempCardPosition[cardId]
+        }
+
+        return
+      }
+
+      case "Game.IdSet": {
+        const { gameId } = action.payload
+
+        draft.game = {
+          id: gameId,
+        }
+
+        return
+      }
+
+      case "Game.IdUnset": {
+        draft.game = undefined
+        draft.piles = []
+        draft.tempCardPosition = {}
+
+        return
+      }
+
+      case "Card.AwaitTransaction": {
+        const { timestamp } = action.payload
+
+        draft.ui.runningLongTransaction.push(timestamp)
+
+        return
+      }
+
+      case "Card.AwaitTransaction.Finished": {
+        const { timestamp } = action.payload
+
+        draft.ui.runningLongTransaction = draft.ui.runningLongTransaction.filter(
+          v => v !== timestamp,
+        )
+
+        return
+      }
+
+      case "Card.ClearTempInfo": {
+        const { timestamp } = action.payload
+
+        Object.entries(draft.tempCardPosition).forEach(([cardId, temp]) => {
+          if (!temp) return
+
+          if (temp.timestamp <= timestamp) {
+            delete draft.tempCardPosition[cardId]
+          }
+        })
+
+        Object.entries(draft.tempCardSurface).forEach(([cardId, temp]) => {
+          if (!temp) return
+
+          if (temp.timestamp <= timestamp) {
+            delete draft.tempCardSurface[cardId]
+          }
+        })
+
+        return
+      }
+
+      default: {
+        const _: never = action
+      }
     }
-  }
-}, initialState)
+  },
+  initialState,
+)
 
 function isPileData(obj: any): obj is Omit<Pile, "id"> {
   if (!obj.cards) {
