bb9a8d3e1465fdf959b148852d9acfc7c9b22152
kazuma1989 <funifuni.1204@gmail.com>
2020-08-15 21:53:53 +0900

    feat: Functions トリガーで ownerGames の整合性を保つ

--- functions/src/api/games.ts
+++ functions/src/api/games.ts
@@ -26,65 +26,37 @@ export const games = functions
 
     const ownerRef = db.collection("users").doc(auth.uid)
 
-    const genData = await db.runTransaction(async t => {
-      const owner = await t.get(ownerRef).then(d => d.data())
+    // 作成の最大数を超えていないかチェック
+    const MAX_COUNT = 3
+    const games = await ownerRef.collection("ownerGames").listDocuments()
+    if (games.length >= MAX_COUNT) {
+      throw new functions.https.HttpsError(
+        "resource-exhausted",
+        `Exceeded max games: ${MAX_COUNT}`,
+        {
+          currentCount: games.length,
+          maxCount: MAX_COUNT,
+        },
+      )
+    }
 
-      // ここに来るのは、Auth と Firestore の連携ミス
-      if (!owner) {
+    // type が対応可能なものかチェックする
+    const genData = await import(`../data/${body?.type}`)
+      .then(m => m.default as () => object)
+      .catch(() => {
         throw new functions.https.HttpsError(
-          "internal",
-          `User data not found: ${ownerRef.id}`,
+          "unimplemented",
+          `Unknown game type: "${body?.type}"`,
           {
-            userId: ownerRef.id,
+            type: body?.type,
           },
         )
-      }
-
-      // 作成の最大数を超えていないかチェック
-      const MAX_COUNT = 3
-      const { ownerGamesCount = 0 } = owner
-      if (ownerGamesCount >= MAX_COUNT) {
-        throw new functions.https.HttpsError(
-          "resource-exhausted",
-          `Exceeded max games: ${MAX_COUNT}`,
-          {
-            currentCount: ownerGamesCount,
-            maxCount: MAX_COUNT,
-          },
-        )
-      }
-
-      t.update(ownerRef, {
-        ownerGamesCount: ownerGamesCount + 1,
       })
-
-      // type が対応可能なものかチェックする
-      const genData = await import(`../data/${body?.type}`)
-        .then(m => m.default as () => object)
-        .catch(() => {
-          throw new functions.https.HttpsError(
-            "unimplemented",
-            `Unknown game type: "${body?.type}"`,
-            {
-              type: body?.type,
-            },
-          )
-        })
-
-      return genData
-    })
 
     // 新しい Game doc を生成する
     const gameRef = await db.collection("games").add({
       owner: ownerRef.id,
       players: [],
-    })
-
-    // User のサブコレクションを更新する
-    await ownerRef.collection("ownerGames").doc(gameRef.id).set({
-      owner: ownerRef.id,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
     })
 
     const bw = db.bulkWriter()
--- functions/src/api/users.ts
+++ functions/src/api/users.ts
@@ -10,12 +10,14 @@ export const userCreate = functions
   .region(functions.config().functions?.region ?? "us-central1")
   .auth.user()
   .onCreate(({ uid, displayName, photoURL }) => {
-    return db.collection("users").doc(uid).set({
-      displayName,
-      photoURL,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-      updatedAt: admin.firestore.FieldValue.serverTimestamp(),
-    })
+    return db
+      .collection("users")
+      .doc(uid)
+      .set({
+        displayName,
+        photoURL,
+        ...timestamp(),
+      })
   })
 
 /**
@@ -27,3 +29,47 @@ export const userDelete = functions
   .onDelete(({ uid }) => {
     return db.collection("users").doc(uid).delete()
   })
+
+/**
+ * Game を作成したらオーナーユーザーにもその情報を持たせる
+ */
+export const userOwnerGamesCreate = functions.firestore
+  .document("games/{gameId}")
+  .onCreate(game$ => {
+    const { id: gameId } = game$
+    const { owner } = game$.data()
+
+    return db
+      .collection("users")
+      .doc(owner)
+      .collection("ownerGames")
+      .doc(gameId)
+      .set({
+        owner,
+        ...timestamp(),
+      })
+  })
+
+/**
+ * Game を削除したらオーナーユーザーからも消しておく
+ */
+export const userOwnerGamesDelete = functions.firestore
+  .document("games/{gameId}")
+  .onDelete(game$ => {
+    const { id: gameId } = game$
+    const { owner } = game$.data()
+
+    return db
+      .collection("users")
+      .doc(owner)
+      .collection("ownerGames")
+      .doc(gameId)
+      .delete()
+  })
+
+function timestamp() {
+  return {
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+  }
+}
