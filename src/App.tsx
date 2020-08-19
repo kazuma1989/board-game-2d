b67b925884588af6ae1b25126ec95136cc656349
kazuma1989 <funifuni.1204@gmail.com>
2020-08-19 10:26:25 +0900

    refactor: あまり意味のなかった lazy App 読み込みをなくした
    エントリーポイントを lazy にしても、ローディングを表示できるだけであまり恩恵はない。
    ローディング表示は scaffold HTML にも書いてあるし。
    ルーティングベースで lazy していく。

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,38 @@
-import React, { useMemo } from "https://cdn.skypack.dev/react"
-import { Provider as ReduxProvider } from "https://cdn.skypack.dev/react-redux"
+import React from "https://cdn.skypack.dev/react"
 import {
   BrowserRouter as Router,
   Route,
   Switch,
 } from "https://cdn.skypack.dev/react-router-dom"
-import { createStore } from "https://cdn.skypack.dev/redux"
 import { AuthListener, AuthRedirect, useAuthLoaded } from "./auth.js"
 import { DebugMenu } from "./DebugMenu.js"
-import { Provider as DexieProvider } from "./dexie.js"
 import { Game } from "./Game.js"
 import { Home } from "./Home.js"
 import { Loading } from "./Loading.js"
 import { mode } from "./mode.js"
 import { NotFound } from "./NotFound.js"
-import {
-  PortalChildrenContainer,
-  Provider as PortalProvider,
-} from "./Portal.js"
-import { reducer } from "./reducer.js"
+import { PortalChildrenContainer } from "./Portal.js"
 import { SignIn } from "./SignIn.js"
 
 export function App() {
-  const store = useMemo(
-    () =>
-      createStore(reducer, undefined, self.__REDUX_DEVTOOLS_EXTENSION__?.()),
-    [],
-  )
-
   return (
-    <ReduxProvider store={store}>
-      <DexieProvider dbName="App">
-        <AuthListener />
+    <>
+      <AuthListener />
 
-        <PortalProvider>
-          <Router>
-            {mode === "debug" && <DebugMenu />}
+      <Router>
+        {mode === "debug" && <DebugMenu />}
 
-            <SwitchPages />
-          </Router>
+        <SwitchPages />
+      </Router>
 
-          <PortalChildrenContainer
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-            }}
-          />
-        </PortalProvider>
-      </DexieProvider>
-    </ReduxProvider>
+      <PortalChildrenContainer
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+        }}
+      />
+    </>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,41 @@
-import React, { Suspense } from "https://cdn.skypack.dev/react"
+import React from "https://cdn.skypack.dev/react"
 import { render } from "https://cdn.skypack.dev/react-dom"
+import { Provider as ReduxProvider } from "https://cdn.skypack.dev/react-redux"
+import { createStore } from "https://cdn.skypack.dev/redux"
+import { App } from "./App.js"
+import { Provider as DexieProvider } from "./dexie.js"
 import { ErrorBoundary } from "./ErrorBoundary.js"
-import { Loading } from "./Loading.js"
+import { firestore, functions } from "./firebase.js"
+import { data } from "./mode.js"
+import { Provider as PortalProvider } from "./Portal.js"
+import { reducer } from "./reducer.js"
 
-const App = React.lazy(() =>
-  Promise.all([import("./App.js"), import("./init-app.js")]).then(
-    ([{ App }]) => ({
-      default: App,
-    }),
-  ),
+if (data === "mock") {
+  console.log("Using emulators: firestore, functions")
+
+  firestore().settings({
+    host: "localhost:5002",
+    ssl: false,
+  })
+
+  functions.useFunctionsEmulator("http://localhost:5001")
+}
+
+const store = createStore(
+  reducer,
+  undefined,
+  self.__REDUX_DEVTOOLS_EXTENSION__?.(),
 )
 
 render(
   <ErrorBoundary>
-    <Suspense fallback={<Loading />}>
-      <App />
-    </Suspense>
+    <PortalProvider>
+      <ReduxProvider store={store}>
+        <DexieProvider dbName="App">
+          <App />
+        </DexieProvider>
+      </ReduxProvider>
+    </PortalProvider>
   </ErrorBoundary>,
   document.getElementById("app"),
 )
